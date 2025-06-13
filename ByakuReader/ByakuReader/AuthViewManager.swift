9117406f0a4cf3a5366722a7b1246589d442fc9b
Arch Patel <73293199+archp4@users.noreply.github.com>
2025-06-13 15:19:39 -0400

    Route Enum added

---
+++ ByakuReader/ByakuReader/AuthViewManager.swift
@@ -0,0 +1,17 @@
+//
+//  AuthViewManager.swift
+//  ByakuReader
+//
+//  Created by Arch Umeshbhai Patel on 2025-06-13.
+//
+
+import Foundation
+
+enum AuthViewManager {
+    case signUp
+    case signIn
+    case home
+}
+
+
+
---
+++ ByakuReader/ByakuReader/HomeViewManager.swift
@@ -0,0 +1,8 @@
+//
+//  HomeViewManager.swift
+//  ByakuReader
+//
+//  Created by Arch Umeshbhai Patel on 2025-06-13.
+//
+
+import Foundation
