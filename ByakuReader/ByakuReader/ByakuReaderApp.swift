d854ab50b1558e2964b9e973231a934f155c506c
Arch Patel <73293199+archp4@users.noreply.github.com>
2025-06-13 15:09:49 -0400

    init

---
+++ ByakuReader/ByakuReader/ByakuReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ByakuReaderApp.swift
+//  ByakuReader
+//
+//  Created by Arch Umeshbhai Patel on 2025-06-13.
+//
+
+import SwiftUI
+
+@main
+struct ByakuReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ByakuReader/ByakuReader/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ByakuReader
+//
+//  Created by Arch Umeshbhai Patel on 2025-06-13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
